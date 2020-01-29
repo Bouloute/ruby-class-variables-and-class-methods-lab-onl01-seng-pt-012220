4b345f8f1b960275a7d2e5390ede0dc15b476532
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-29 16:13:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
+  @@count_of_songs = 0
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count_of_songs += 1
+  end
+
+  def count 
+    @@count_of_songs
   end
 end
